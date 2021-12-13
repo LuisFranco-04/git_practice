e1fe2312c92185f9103a4b459c4544ad31e1de68
Luis Franco <capitanmano@gmail.com>
2021-12-12 19:49:58 -0700

    Add random messages generator

--- hello.js
+++ hello.js
@@ -1,1 +1,6 @@
-console.log('Hello World')
+let subject = ['you', 'she', 'him', 'they']
+let adjectives = ['generous', 'funny', 'weird', 'cool']
+function createPhrase(){
+    return `${subject[Math.floor(Math.random() * 4)]} are/is ${adjectives[Math.floor(Math.random() * 4)]}`
+}
+console.log(createPhrase())
