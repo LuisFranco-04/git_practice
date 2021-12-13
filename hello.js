58d25a25ab6a39726f969ac85fb233c260acd1a0
Luis Franco <capitanmano@gmail.com>
2021-12-13 14:48:33 -0700

    Investing Calculator

--- hello.js
+++ hello.js
@@ -1,7 +1,41 @@
-let subject = ['you', 'she', 'him', 'they']
-let adjectives = ['generous', 'funny', 'weird', 'cool']
-function createPhrase(){
-    return `${subject[Math.floor(Math.random() * 4)]} are/is ${adjectives[Math.floor(Math.random() * 4)]}`
+let salaries = [134000, 164000, 192000, 250000]
+
+const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2
+  })
+
+function calculateGrowth(initial, yearlyReturn, salaries, years){
+    const monthlyReturn = (yearlyReturn / 12) + 1
+    let months = years * 12
+    let currentCap = initial
+    let x = 0
+    for (let i = 0; i <= months; i++){
+        let currentYear = i / 12
+        let salaryIndex = 0
+        if (currentYear < 3){
+            salaryIndex = 0
+        } else if (currentYear < 7){
+            salaryIndex = 1
+            currentYear === 3 ? console.log(`Year 3: ${formatter.format(currentCap)} Age: ${25 + currentYear}`) : currentCap = currentCap
+        } else if (currentYear < 11){
+            salaryIndex = 2
+            currentYear === 7 ? console.log(`Year 7: ${formatter.format(currentCap)} Age: ${25 + currentYear}`) : currentCap = currentCap 
+        } else if (currentYear >= 12){
+            salaryIndex = 3
+            currentYear === 15 ? console.log(`Year 15: ${formatter.format(currentCap)} Age: ${25 + currentYear}`) : currentCap = currentCap
+            currentYear === 17 ? console.log(`Year 17: ${formatter.format(currentCap)} Age: ${25 + currentYear}`) : currentCap = currentCap
+            currentYear === 12 ? console.log(`Year 12: ${formatter.format(currentCap)} Age: ${25 + currentYear}`) : currentCap = currentCap
+        }
+        let currentMonthFinal = (currentCap * monthlyReturn) + ((salaries[salaryIndex] / 36))
+        currentCap = currentMonthFinal
+        if (currentCap >= 1000000000 && x === 0){
+            console.log(`You are a billionaire!! Age: ${25 + currentYear}, year ${currentYear}`)
+            x = 1
+        }
+    }
+    console.log(`Final cap: ${formatter.format(currentCap)}`)
 }
-// This is new
-console.log(createPhrase())
+
+calculateGrowth(10000, 0.5, salaries, 21)
